f5af9e539b958fd6c9ae0aff1ab466abb5d3cba4
Phong Nguyen <NGUYENP@siopergaud.fr>
2024-09-19 16:22:01 +0200

    premier commit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<!--    <link rel="stylesheet" href="./assets/css/darkly-bootstrap.min.css">-->
+    <title>Accueil</title>
+</head>
+<?php require_once "./pages/header.php"?>
+<body>
+    <h1 class="text-center">Yokoso watashi wa no Portfolio</h1>
+</body>
+</html>
---
+++ public/pages/header.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Portfolio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarColor01">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Accueil
+                        <span class="visually-hidden">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">A propos de moi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Compétences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Expérience</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Projet</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
